15b1d50350293443d7dbf7724bb817e968b05330
Wolganblack <ALFONSO@DESKTOP-7VNR7GU>
2023-04-14 14:29:58 -0600

    Prueba en Pythob

---
+++ capturar_num_y_sumar.py
@@ -0,0 +1,15 @@
+print("calculator program")
+first_number =( input("first number: "))
+second_number = input("second number: ")
+a=int(first_number)
+b=int(second_number)
+suma=a+b
+print(first_number + second_number)# hay que parserar datos si no python los tomas como string
+#Solucion seria
+print(suma)
+
+print("my message")
+ 
+
+
+
---
+++ fecha_actual.py
@@ -0,0 +1,5 @@
+from datetime import date
+
+fecha_actual=date.today()
+
+print(fecha_actual)
